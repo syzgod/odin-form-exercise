71de0c7d3db1639c5f9607136328d388b62b0ce4
syzgod <viktor.berczeli@gmail.com>
2022-10-24 00:35:19 +0200

    Basic styling

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      fasthand: ["Fasthand", "cursive", "sans"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
